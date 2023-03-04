cd55d362ff178bbe1fe6d9d2bbe2494540a23f32
John Lee <john.lee@ufl.edu>
2023-03-04 02:14:27 -0500

    update3

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ var questionsArr =
         question:"How many NBA Championships did Michael Jordan win?",
         answer: "6",
         options: [
-            "6",
             "8",
+            "6",
         ]
     },
     {
@@ -41,64 +41,129 @@ var questionsArr =
         ]
 
     },
+    {
+        question:"Which NBA team did Charles Barkley play for?",
+        answer: "Jacksonville Jaguars",
+        options: [
+            "Orlando Magic",
+            "Houston Rockets",
+        ] 
+    }
 ]
 
 var getQuiz = document.getElementById('quiz')
+if (previousScore){
+p = document.createElement('p'); p.id = 'quizQuestion'
+getQuiz.appendChild(p)
+var previousScore = localStorage.getItem('previous-score',finalScore)
+quizQuestion.textContent = "Previous Score:"+ previousScore + "%"
 var button = document.createElement('button'); button.id = 'start-quiz'
 getQuiz.appendChild(button)
 button.textContent="Start Quiz!"
 var startQuiz = document.getElementById('start-quiz')
+}else{
+    p = document.createElement('p'); p.id = 'quizQuestion'
+    getQuiz.appendChild(p)
+    quizQuestion.textContent = ""
+    var button = document.createElement('button'); button.id = 'start-quiz'
+    getQuiz.appendChild(button)
+    button.textContent="Start Quiz!"
+    var startQuiz = document.getElementById('start-quiz')
+}
 
 startQuiz.onclick = function runQuiz (){
     getQuiz.removeChild(startQuiz)
-     // Question
-    p = document.createElement('p'); p.id = 'problem'
-    getQuiz.appendChild(p)
-    
+    var x = 0
+    var score = 0
+
+    // Question
+    var questionText = questionsArr[x].question
+    quizQuestion.textContent = questionText
 
     //Buttons
-    for (var x = 0; x < 2; x++){
-        var button = document.createElement('button')
-        getQuiz.appendChild(button)
+    var div = document.createElement('div')
+    getQuiz.appendChild(div)
+    var button = document.createElement('button'); button.id = 'buttonZero'
+    div.appendChild(button)
+    var button = document.createElement('button'); button.id = 'buttonOne'
+    div.appendChild(button)
+    buttonZero.textContent = questionsArr[x].options[0]
+    buttonOne.textContent = questionsArr[x].options[1]
     
-        //question and choices
-        for (var i = 0; i < questionsArr.length; i++) {
-            var choicesText = questionsArr[i].options[x]
-            var questionText = questionsArr[i].question
-            problem.textContent = questionText
-            button.textContent = choicesText
-            
-            //Advance question
-            var btns = document.querySelectorAll('.buttons')
-
-            button.onclick = function advance(){
-                i=i-1
-                problem.textContent = "advanced"
-                button.textContent = choicesText
-                time.textContent = 30
-            }
-            
-            //correct answer
-        
-            // var correctAnswer = questionsArr[i].answer
-            // var score = 0
-           
-            
-        }
-    }
-
+    
     // Time
     p = document.createElement('p'); p.id = 'time'
     getQuiz.appendChild(p)
     p.textContent = '30'
+   
+    //Advance
+    buttonZero.onclick = function advance(){
+        x=x+1
+        if (x < 6){
+        time.textContent = '30'
+        var questionText = questionsArr[x].question
+        quizQuestion.textContent = questionText
+        buttonZero.textContent = questionsArr[x].options[0]
+        buttonOne.textContent = questionsArr[x].options[1]
+        }else{
+            var finalScore = Math.round(score /questionsArr.length * 100);
+            localStorage.setItem('previous-score',finalScore)
+            var previousScore = localStorage.getItem('previous-score',finalScore)
+            quizQuestion.textContent = "Previous Score:" + previousScore + "%"
+            div.removeChild(buttonOne)
+            div.removeChild(buttonZero)
+            getQuiz.removeChild(div)
+            getQuiz.removeChild(time)
+            var button = document.createElement('button'); button.id = 'start-quiz'
+            getQuiz.appendChild(button)
+            button.textContent="Start Quiz!"
+            var startQuiz = document.getElementById('start-quiz')
+            startQuiz.onclick = runQuiz ()
+        }
+    }
+    buttonOne.onclick = function advance(){
+        x=x+1
+        score = score + 1
+        console.log(score)
+        if (x < 6){
+        time.textContent = '30'
+        var questionText = questionsArr[x].question
+        quizQuestion.textContent = questionText
+        buttonZero.textContent = questionsArr[x].options[0]
+        buttonOne.textContent = questionsArr[x].options[1]
+        }else{
+            var finalScore = Math.round(score /questionsArr.length * 100);
+            localStorage.setItem('previous-score',finalScore)
+            var previousScore = localStorage.getItem('previous-score',finalScore)
+            quizQuestion.textContent = "Previous Score:" + previousScore + "%"
+            div.removeChild(buttonOne)
+            div.removeChild(buttonZero)
+            getQuiz.removeChild(div)
+            getQuiz.removeChild(time)
+            var button = document.createElement('button'); button.id = 'start-quiz'
+            getQuiz.appendChild(button)
+            button.textContent="Start Quiz!"
+            var startQuiz = document.getElementById('start-quiz')
+            startQuiz.onclick = runQuiz ()
+        }
+    }
+    
 
     // Countdown and reset
     var intervalId = setInterval(function() {
         var seconds = Number(p.textContent)-1
         if(seconds === -1) {
+            x=x+1
+            time.textContent = '30'
+            var questionText = questionsArr[x].question
+            quizQuestion.textContent = questionText
+            buttonZero.textContent = questionsArr[x].options[0]
+            buttonOne.textContent = questionsArr[x].options[1]
             clearInterval(intervalId)
         } else {
             p.textContent = seconds
         }
     },1000)
 }
+
+
